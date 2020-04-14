a809b996f32047011093a2eeb914841567f3fa35
Vineet <Vineet@Vineet-PC.mshome.net>
2020-04-14 14:51:01 +0530

    word length in csv file

--- Beam/src/main/java/Practice_Basic/ReadCSV.java
+++ Beam/src/main/java/Practice_Basic/ReadCSV.java
@@ -1,10 +1,14 @@
 package Practice_Basic;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.Flatten;
 import org.apache.beam.sdk.transforms.MapElements;
 import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.transforms.SimpleFunction;
@@ -30,21 +34,31 @@ public class ReadCSV {
 	public static void main(String[] args) {
 		PipelineOptions options= PipelineOptionsFactory.create();
 		Pipeline pipeline=Pipeline.create(options);
-		PCollection<String> lines=pipeline.apply("Read from file",TextIO.read().from("D:\\beam\\test.csv"));
+		PCollection<String> lines=pipeline.apply("Read from file",TextIO.read().from("D:\\beam\\test1.csv"));
 
-		PCollection<Integer> words = (PCollection<Integer>) lines.apply(ParDo.of(new DoFn<String, Integer>() {
+		
+		PCollection words =lines.apply(MapElements.via(new SimpleFunction<String, List<String>>(){
+			@Override
+			public List<String> apply(String input){
+				return Arrays.asList(input.split(","));
+			}
+			}));
+		PCollection<String> word =(PCollection<String>) words.apply(Flatten.iterables());
+		
+		PCollection<Integer> wordLength = (PCollection<Integer>) word.apply(ParDo.of(new DoFn<String, Integer>() {
 			@ProcessElement
 			public void processElement(@Element String word, OutputReceiver<Integer> out) {
+				if(word.length()>1) {
 				out.output(word.length());
-			}
+			}}
 		}));
 
-		words.apply(MapElements.via(new SimpleFunction<Integer,String>(){
+		wordLength.apply(MapElements.via(new SimpleFunction<Integer,String>(){
 			@Override
 			public String apply(Integer input) {
-				return String.format("%s",input);
+				return String.format("%d",input);
 			}
-		})).apply(TextIO.write().to("D:\\beam\\wordlength").withSuffix(".txt"));
+		})).apply(TextIO.write().to("D:\\beam\\wordlength12").withSuffix(".txt"));
 		pipeline.run().waitUntilFinish();
 	}
 
