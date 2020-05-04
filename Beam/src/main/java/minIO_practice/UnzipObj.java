f4c50960399f4f7de5fed69a0379498827a0811e
Vineet <Vineet@Vineet-PC>
2020-05-04 17:32:55 +0530

    optimized code

--- Beam/src/main/java/minIO_practice/UnzipObj.java
+++ Beam/src/main/java/minIO_practice/UnzipObj.java
@@ -29,6 +29,7 @@ import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
 import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
 import org.apache.commons.compress.utils.IOUtils;
 
+import com.amazonaws.AmazonServiceException;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
@@ -44,19 +45,23 @@ import io.minio.errors.MinioException;
 
 
 public class UnzipObj {
-	static AWSCredentials awsCredentials = new BasicAWSCredentials("cred1", "cred2");
 	
 	 public static class unzip extends DoFn<FileIO.ReadableFile, String> {
 		 String des;
-		 unzip(String des){
+		 String accessKey;
+		 String secretKey;
+
+		 unzip(String des,String cred1, String cred2){
 			 this.des=des;
-			
+			 accessKey=cred1;
+			 secretKey=cred2;
+
 		 }
 		 @DoFn.ProcessElement
 		 public void processElement(@Element ReadableFile element, DoFn.OutputReceiver<String> receiver) throws IOException {
 			 AmazonS3 s3client = AmazonS3ClientBuilder
 					  .standard()
-					  .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+					  .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey,secretKey)))
 					  .withRegion(Regions.US_WEST_2)
 					  .build();
 			 System.out.println("Inside function ");
@@ -64,6 +69,9 @@ public class UnzipObj {
 			 InputStream is = Channels.newInputStream(element.open());
 			 TarArchiveInputStream tis = new TarArchiveInputStream(is);
 			 TarArchiveEntry tarEntry = tis.getNextTarEntry();
+			 if(!exists(s3client,des+tarEntry.getName()))
+			 {
+				
 		        while (tarEntry != null) {
 		            byte[] btoRead = new byte[1024];
 		            ByteArrayOutputStream bout = new ByteArrayOutputStream();
@@ -87,6 +95,7 @@ public class UnzipObj {
 		       		bais.close();
 		            tarEntry = tis.getNextTarEntry();
 		        }
+			 }
 		        System.out.println("done");
 		        tis.close();
 		      		tis.close();
@@ -97,6 +106,19 @@ public class UnzipObj {
 			 
 	 }
 	 }
+	 
+		
+		  static boolean exists(AmazonS3 s3,String path) {
+			    try {
+			        s3.getObjectMetadata("radiantlabs", path); 
+			    } catch(AmazonServiceException e) {
+			    	System.out.println("no key");
+			        return false;
+			    }
+			    System.out.println("key present");
+			    return true;
+			}
+	 
 	  
 	
 	public interface CustomerOfflineDataOptions extends PipelineOptions {
@@ -105,6 +127,10 @@ public class UnzipObj {
 		void setInputFile(String file);
 		String getOutputFile();
 		void setOutputFile(String des);
+		String getAccessKey();
+		void setAccessKey(String cred1);
+		String getSecretKey();
+		void setSecretKey(String cred2);
 		
 	}
 
@@ -112,14 +138,13 @@ public class UnzipObj {
 		CustomerOfflineDataOptions options = PipelineOptionsFactory.fromArgs(args).withValidation()
 				.as(CustomerOfflineDataOptions.class);
 		
-		
+		AWSCredentials awsCredentials = new BasicAWSCredentials(options.getAccessKey(),options.getSecretKey());
 		options.as(AwsOptions.class).setAwsCredentialsProvider(new AWSStaticCredentialsProvider(awsCredentials));
 		options.as(AwsOptions.class).setAwsRegion("us-west-2");
 		Pipeline pipeline = Pipeline.create(options);
-		
 		PCollection input = pipeline.apply("ReadInput", FileIO.match().filepattern(options.getInputFile()))
 				.apply(FileIO.readMatches().withCompression(Compression.GZIP));
-		input.apply(ParDo.of(new unzip(options.getOutputFile())));
+		input.apply(ParDo.of(new unzip(options.getOutputFile(),options.getAccessKey(),options.getSecretKey())));
 
 		pipeline.run().waitUntilFinish();
 	}
